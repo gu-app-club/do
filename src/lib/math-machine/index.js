1722e6810d2eb4938ebf11867fb924b16ab2bfb3
Flaqueeau <econrad@zagmail.gonzaga.edu>
2018-03-08 15:31:17 -0800

    Added shell functions for math machine

---
+++ src/lib/math-machine/index.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Functions we need:
+ * 1. Take object, eventually turn it into a vector
+ * 2. Calculate distance between arbitrary vectors
+ * 3. Given a current point and a bunch of goal vectors, 
+ *      return an ordered / sorted list of the closest vectors
+ * 
+ */
+
+export function jazz() {
+
+}
+
+export function buzzjingles() {
+    
+}
---
+++ src/lib/math-machine/math.test.js
@@ -0,0 +1,5 @@
+import { jazz, buzzjingles } from "./index.js";
+
+jazz();
+
+buzzjingles();
